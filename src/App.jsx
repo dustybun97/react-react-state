63d1572292a175c224f9efa0c66aa7bd7a6430a1
podusit <d.rimdusit@gmail.com>
2024-10-23 13:40:41 +0700

    feat:add greeting button with react state

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
 import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [greeting, setGreeting] = useState("Greeting Message");
+  const handleGreeting = (message) => {
+    setGreeting(message);
+  };
+
   return (
     <div className="App">
-      <div className="greeting-container">Greeting Message</div>
+      <div className="greeting-container">{greeting}</div>
       <div className="buttons">
-        <button>สวัสดี!</button>
-        <button>Hi!</button>
-        <button>你好!</button>
+        <button onClick={() => handleGreeting("สวัสดี!")}>สวัสดี!</button>
+        <button onClick={() => handleGreeting("Hi!")}>Hi!</button>
+        <button onClick={() => handleGreeting("你好!")}>你好!</button>
       </div>
     </div>
   );
